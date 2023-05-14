511cfe0c469715d89a079552d4b658c70964db30
RIPUNJAY287 <kripunjay81@gmail.com>
2023-05-14 19:40:19 +0530

    netlify deploy

--- src/components/baseUrl.js
+++ src/components/baseUrl.js
@@ -1,3 +1,5 @@
 //const baseUrl = "https://fooddude-merch-backend.herokuapp.com";
-const baseUrl = "https://rstore-backend.herokuapp.com";
+//const baseUrl = "https://rstore-backend.herokuapp.com";
+//const baseUrl = "https://idyllic-caramel-454c62.netlify.app";
+const baseUrl = "https://rjstore-backend-01.vercel.app";
 export default baseUrl;
